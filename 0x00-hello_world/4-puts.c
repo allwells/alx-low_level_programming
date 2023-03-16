02b8c7a6be8d6ab4839ae7b42d7100aa15c9a8c7
Allwell Onen <aleenfestus@gmail.com>
2023-03-16 13:32:02 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  * Return: 0
  */
 
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
