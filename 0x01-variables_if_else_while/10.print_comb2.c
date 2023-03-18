89bcbd0c595038295dc2bfa52effd68136e7dd54
Allwell Onen <aleenfestus@gmail.com>
2023-03-18 12:30:45 +0100

    Print combination 2

---
+++ 0x01-variables_if_else_while/10.print_comb2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b;
+
+	while (a <= 9)
+	{
+		b = 0;
+
+		while (b <= 9)
+		{
+			putchar('0' + a);
+			putchar('0' + b);
+
+			if (a != 9 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
